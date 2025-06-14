35ac7b013d8b8c92928ea84cc856a6ad22eb57ea
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-06-14 15:47:27 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Oil_level_glass
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(700, 360);
+            Font = new Font("GOST type B", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "MainForm";
+            Text = "Окошко для контроля уровня масла";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
