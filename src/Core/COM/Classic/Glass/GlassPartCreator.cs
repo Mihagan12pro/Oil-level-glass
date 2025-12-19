9ec56a25e06da8cf6a627c12fb2d618ec6d5194a
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-12-19 12:35:07 +0300

    feat: new extension (LineSegmentExtension), GlassPartCreator::AddSketch1() as able to add 4 lines to sketch.

--- src/Core/COM/Classic/Glass/GlassPartCreator.cs
+++ src/Core/COM/Classic/Glass/GlassPartCreator.cs
@@ -2,6 +2,7 @@
 using Oil_level_glass.COM.Extensions;
 using Oil_level_glass.Core.Classic.Glass;
 using Oil_level_glass.Model.Data.Entities.Parts.Classic;
+using Shared.Points;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,14 +20,33 @@ namespace Oil_level_glass.COM.Classic.Glass
 
         public void AddSketch1()
         {
+            Point2DCrossApi leftBottomPoint = new Point2DCrossApi(-1, 0);
+            Point2DCrossApi rightBottomPoint = new Point2DCrossApi(1, 0);
+
+            Point2DCrossApi leftTopPoint = new Point2DCrossApi(-1, 30);
+            Point2DCrossApi rightTopPoint = new Point2DCrossApi(1, 30);
+
             _sketch1 = ModelContainer.Sketchs.Add();
             _sketch1.Plane = Part7?.GetPlaneXOY();
             _sketch1.Update();
 
             IDrawingContainer sketch1DrawingContainer = _sketch1.GetDrawingContainer();
 
-            ILineSegment lineSegment = sketch1DrawingContainer.LineSegments.Add();
-         
+            ILineSegment bottomLine = sketch1DrawingContainer.LineSegments.Add();
+            bottomLine.SetPoints(leftBottomPoint, rightBottomPoint);
+            bottomLine.Update();
+
+            ILineSegment topLine = sketch1DrawingContainer.LineSegments.Add();
+            topLine.SetPoints(leftTopPoint, rightTopPoint);
+            topLine.Update();
+
+            ILineSegment rightLine = sketch1DrawingContainer.LineSegments.Add();
+            rightLine.SetPoints(rightBottomPoint, rightTopPoint);
+            rightLine.Update();
+
+            ILineSegment leftLine = sketch1DrawingContainer.LineSegments.Add();
+            leftLine.SetPoints(leftBottomPoint, leftTopPoint);
+            leftLine.Update();
 
             _sketch1.EndEdit();
         }
--- src/Tests/ComTests.cs
+++ src/Tests/ComTests.cs
@@ -1,5 +1,7 @@
 ﻿using Oil_level_glass.COM;
+using Oil_level_glass.COM.Classic.Glass;
 using Oil_level_glass.Core;
+using Oil_level_glass.Core.Classic.Glass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,14 @@ namespace Oil_level_glass.Tests
     public class ComTests
     {
         [Fact()]
-        public void Test1()
+        public void TestGlassCreator()
         {
-            
+            ComGlassPartCreatorsFactory partCreatorsFactory = new ComGlassPartCreatorsFactory();
+
+            IGlassPartCreator glassPartCreator = partCreatorsFactory.GetCreator();
+
+            glassPartCreator.Initialize();
+            glassPartCreator.AddSketch1();
         }
     }
 }
