85147498200c3b07bd9088b9c3ce74a343ef0b47
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-01-06 11:42:10 +0300

    feat: create oil level glass assembly

--- src/Core/COM/Classic/OilLevelGlass/OilLevelGlassPartCreator.cs
+++ src/Core/COM/Classic/OilLevelGlass/OilLevelGlassPartCreator.cs
@@ -1,5 +1,6 @@
 ﻿using Kompas6Constants3D;
 using KompasAPI7;
+using Oil_level_glass.COM.Extensions.Containers;
 using Oil_level_glass.COM.Extensions.ModelObjects;
 using Oil_level_glass.COM.Extensions.V7;
 using Oil_level_glass.Core.Classic.OilLevelGlass;
@@ -27,15 +28,12 @@ namespace Oil_level_glass.COM.Classic.OilLevelGlass
         private IFace[]? _bottomRubberStripFaces;
         private IEdge[]? _bottomRubberStripEdges;
 
-
-        public void AddAllParts()
-        {
-            throw new NotImplementedException();
-        }
-
         public void AddGlass()
         {
-            throw new NotImplementedException();
+           _glass = AssemblyPart7.AddPartByModel(PartModel.GlassModel);
+
+           _glassFaces =_glass.GetFaces();
+           _glassEdges =_glass.GetEdges();
         }
 
         public void AddHousing()
@@ -49,32 +47,31 @@ namespace Oil_level_glass.COM.Classic.OilLevelGlass
 
         public void AddTopRubberStrip()
         {
-            throw new NotImplementedException();
-        }
-
-        public void AddRubberStrips()
-        {
-            throw new NotImplementedException();
+            AddRubberStrip(out _topRubber, out _topRubberStripFaces, out _topRubberStripEdges);
         }
 
         public void AddBottomRubberStrip()
         {
-            _bottomRubber = AssemblyPart7.AddPartByModel(PartModel.RubberStripModel);
-
-            _bottomRubberStripFaces = _bottomRubber.GetFaces();
-            _bottomRubberStripEdges = _bottomRubber.GetEdges();
+            AddRubberStrip(out _bottomRubber, out _bottomRubberStripFaces, out _bottomRubberStripEdges);
         }
 
         public void Assemble()
         {
             AddHousing();
-
             AddBottomRubberStrip();
 
-            MakeHousingAndBottomStripConcentric();
+            ConnectHousingAndBottomStrip();
+
+            AddGlass();
+
+            ConnectBottomStripAndGlass();
+
+            AddTopRubberStrip();
+
+            ConnectTopStripAndGlass();
         }
 
-        private void MakeHousingAndBottomStripConcentric()
+        private void ConnectHousingAndBottomStrip()
         {
             IMateConstraint3D concentric = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Concentric);
 
@@ -101,6 +98,151 @@ namespace Oil_level_glass.COM.Classic.OilLevelGlass
             concentric.BaseObject2 = housingEdge;
 
             concentric.Update();
+
+            IMateConstraint3D coincidence = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Coincidence);
+
+            IFace stripFace = _bottomRubberStripFaces!
+                .Where(f =>f.IsPlanar &&  
+                    f.GetEdges()
+                        .Where(
+                            e => e.ToPoint() == new Point3DCrossApi(
+                                stripModel.ExternalDiameter / 2, 
+                                0, 
+                                -stripModel.Height / 2)
+                            ).FirstOrDefault() != null)
+                .First();
+
+            coincidence.BaseObject1 = stripFace;
+            coincidence.BaseObject2 = housingEdge;
+            coincidence.Update();
+        }
+
+        private void ConnectBottomStripAndGlass()
+        {
+            IMateConstraint3D concentric = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Concentric);
+
+            var glassModel = PartModel.GlassModel;
+            var stripModel = PartModel.RubberStripModel;
+
+            IEdge stripEdge = _bottomRubberStripEdges!.
+                Where(e =>
+                    e.ToPoint() == new Point3DCrossApi(
+                        stripModel.ExternalDiameter / 2,
+                        0,
+                        stripModel.Height / 2))
+                .First();
+
+            IFace glassCylindricFace = _glassFaces!.
+                Where(f => f.IsCylinder && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            glassModel.ExternalDiameter * 0.5,
+                            0,
+                            glassModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            concentric.BaseObject1 = glassCylindricFace;
+            concentric.BaseObject2 = stripEdge;
+
+            concentric.Update();
+
+
+            IMateConstraint3D coincidence = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Coincidence);
+
+            IFace glassPlanarFace = _glassFaces!.
+                Where(f => f.IsPlanar && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            glassModel.ExternalDiameter * 0.5,
+                            0,
+                            -glassModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+
+            IFace stripFace = _bottomRubberStripFaces!.
+                Where(f => f.IsPlanar && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            stripModel.ExternalDiameter * 0.5,
+                            0,
+                            stripModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            coincidence.BaseObject1 = glassPlanarFace;
+            coincidence.BaseObject2 = stripFace;
+
+            coincidence.Update();
+        }
+
+        private void ConnectTopStripAndGlass()
+        {
+            IMateConstraint3D concentric = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Concentric);
+
+            var glassModel = PartModel.GlassModel;
+            var stripModel = PartModel.RubberStripModel;
+
+            IFace stripCylindricFace = _topRubberStripFaces!.
+                Where(f => f.IsCylinder && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            stripModel.ExternalDiameter * 0.5,
+                            0,
+                            stripModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            IFace glassCylindricFace = _glassFaces!.
+                Where(f => f.IsCylinder && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            glassModel.ExternalDiameter * 0.5,
+                            0,
+                            glassModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            concentric.BaseObject1 = glassCylindricFace;
+            concentric.BaseObject2 = stripCylindricFace;
+
+            concentric.Update();
+
+            IMateConstraint3D coincidence = AssemblyPart7.MateConstraints.Add(MateConstraintType.mc_Coincidence);
+
+            IFace stripPlanarBottomFace = _topRubberStripFaces!.
+                Where(f => f.IsPlanar && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            stripModel.ExternalDiameter * 0.5,
+                            0,
+                            stripModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            IFace glassPlanarTopFace = _glassFaces!.
+                Where(f => f.IsPlanar && f.GetEdges()
+                    .Where(e =>
+                        e.ToPoint() == new Point3DCrossApi(
+                            glassModel.ExternalDiameter * 0.5,
+                            0,
+                            glassModel.Height / 2)
+                        ).FirstOrDefault() != null)
+                .First();
+
+            coincidence.BaseObject1 = stripPlanarBottomFace;
+            coincidence.BaseObject2 = glassPlanarTopFace;
+
+            coincidence.Update();
+        }
+
+        private void AddRubberStrip(out IPart7 part, out IFace[]? faces, out IEdge[]? edges)
+        {
+            part = AssemblyPart7.AddPartByModel(PartModel.RubberStripModel);
+
+            faces = part.GetFaces();
+            edges = part.GetEdges();
         }
     }
 }
--- src/Core/Core/Classic/OilLevelGlass/IOilLevelGlassPartCreator.cs
+++ src/Core/Core/Classic/OilLevelGlass/IOilLevelGlassPartCreator.cs
@@ -10,24 +10,14 @@ namespace Oil_level_glass.Core.Classic.OilLevelGlass
         void AddGlass();
 
         /// <summary>
-        /// Add only top rubber strip
+        /// Add top rubber strip
         /// </summary>
         void AddTopRubberStrip();
 
         /// <summary>
-        /// Add only bottom rubber strip
+        /// Add bottom rubber strip
         /// </summary>
         void AddBottomRubberStrip();
-
-        /// <summary>
-        /// Add two rubber strips
-        /// </summary>
-        void AddRubberStrips();
-
-        /// <summary>
-        /// Add all parts
-        /// </summary>
-        void AddAllParts();
 
         /// <summary>
         /// Assemble all parts
