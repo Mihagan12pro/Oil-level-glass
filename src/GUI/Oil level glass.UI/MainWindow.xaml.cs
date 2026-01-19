7fcde8f65928f038e27af26831457b5597e03b3a
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-01-19 17:14:57 +0300

    feat: return some old projects.

---
+++ src/GUI/Oil level glass.UI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using KompasWPF;
+
+namespace Oil_level_glass_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : KompasWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //Wizard3DWindow wizard3DWindow = new Wizard3DWindow();
+  
+            //wizard3DWindow.Owner = this;
+
+            //wizard3DWindow.ShowDialog();
+        }
+    }
+}
