014e19df7ae12e8dba73ac34e76365160b69d01b
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-01-23 23:50:42 +0300

    feat: modify AboutWindow (use DocumentViewer instead of TextBlock)

--- src/GUI/Oil level glass.UI/Windows/About/AboutWindow.xaml.cs
+++ src/GUI/Oil level glass.UI/Windows/About/AboutWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using KompasWPF;
+using Oil_level_glass.ViewModels.Windows.About;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Controls;
+using System.Windows.Xps.Packaging;
 
 namespace Oil_level_glass_UI.Windows.About
 {
@@ -10,11 +15,19 @@ namespace Oil_level_glass_UI.Windows.About
         public AboutWindow()
         {
             InitializeComponent();
+
+            DataContext = new AboutViewModel("../../../Resources/Texts/About.txt");
         }
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void btClose_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Grid_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            XpsDocument doc = new XpsDocument("../../../Resources/Texts/About.xps", FileAccess.Read);
+            dvAbout.Document = doc.GetFixedDocumentSequence();
         }
     }
 }
--- src/GUI/Oil level glass.ViewModels/Windows/About/AboutViewModel.cs
+++ src/GUI/Oil level glass.ViewModels/Windows/About/AboutViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace Oil_level_glass.ViewModels.Windows.About
+﻿using System.IO;
+
+namespace Oil_level_glass.ViewModels.Windows.About
 {
     public class AboutViewModel : ViewModelBase
     {
+        public string AboutText 
+            => File.ReadAllText(_aboutPath);
+
+        private readonly string _aboutPath;
+
+        public AboutViewModel(string aboutPath)
+        {
+            _aboutPath = aboutPath;
+        }
     }
 }
