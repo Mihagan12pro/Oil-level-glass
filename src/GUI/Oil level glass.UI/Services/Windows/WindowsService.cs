68855d940e830a331de81cf97bec32ccf516efb6
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-01-31 11:53:31 +0300

    refactor: put all button styles to the ButtonStyles folder

--- src/GUI/Oil level glass.UI/Services/Windows/WindowsService.cs
+++ src/GUI/Oil level glass.UI/Services/Windows/WindowsService.cs
@@ -1,10 +1,5 @@
 ﻿using Oil_level_glass.ViewModels.Services.Windows;
 using Oil_level_glass_UI.Windows.About;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Oil_level_glass_UI.Services.Windows
 {
@@ -13,6 +8,7 @@ namespace Oil_level_glass_UI.Services.Windows
         public void ShowAboutWindow()
         {
             AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.ShowInTaskbar = false;
 
             aboutWindow.ShowDialog();
         }
