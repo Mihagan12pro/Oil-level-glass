d0e7b2133bffe48bbe9dcfcca06275a1c3b112b9
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-02-01 17:11:30 +0300

    feat: new user control - MaterialChooser

---
+++ src/GUI/KompasWPF/UserControls/MaterialChooser.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace KompasWPF.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MaterialChooser.xaml
+    /// </summary>
+    public partial class MaterialChooser : UserControl
+    {
+        public MaterialChooser()
+        {
+            InitializeComponent();
+        }
+    }
+}
