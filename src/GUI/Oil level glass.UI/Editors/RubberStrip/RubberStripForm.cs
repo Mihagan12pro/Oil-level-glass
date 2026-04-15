8e71a0a88d1ebd88c4d84ae186c2eec878537034
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-04-16 00:13:06 +0300

    feat: add rubber strip editor form

---
+++ src/GUI/Oil level glass.UI/Editors/RubberStrip/RubberStripForm.cs
@@ -0,0 +1,24 @@
+﻿using Oil_level_glass.Model.Data.Entities.Parts.Classic;
+using Oil_level_glass.UI.Abstractions.Editors.RubberStrip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oil_level_glass.UI.Editors.RubberStrip
+{
+    public partial class RubberStripEditorForm : Form, IRubberStripForm
+    {
+        public RubberStripEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        public RubberStripModel Model { get; set; }
+    }
+}
